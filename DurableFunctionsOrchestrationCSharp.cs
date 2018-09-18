572acc25b2713e432d0bde7d62559bc655cdcf17
timmyreilly <timreilly@live.com>
2018-09-18 16:22:46 -0700

    working again all the content of csv is hitting bundle

--- DurableFunctionsOrchestrationCSharp.cs
+++ DurableFunctionsOrchestrationCSharp.cs
@@ -26,16 +26,11 @@ namespace Company.Function
             var content3 = context.WaitForExternalEvent<string>("ProductInformation");
 
             var something = await Task.WhenAll(content1, content2, content3);
+            List<string> bundle = new List<string>(); 
+            bundle.AddRange(something); 
+            bundle.Add(prefix); 
 
-
-            // Replace "hello" with the name of your Durable Activity Function.
-            // outputs.Add(await context.CallActivityAsync<string>("DurableFunctionsOrchestrationCSharp_Hello", "Tokyo"));
-            // outputs.Add(await context.CallActivityAsync<string>("DurableFunctionsOrchestrationCSharp_Hello", "Seattle"));
-            // outputs.Add(await context.CallActivityAsync<string>("DurableFunctionsOrchestrationCSharp_Hello", "London"));
-            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
-            // outputs.AddRange([fileAReceived, fileBReceived, fileCReceived]);  
-
-            await context.CallActivityAsync("Bundle", something);
+            await context.CallActivityAsync("Bundle", bundle);
 
             return prefix;
         }
@@ -46,11 +41,13 @@ namespace Company.Function
             log.LogInformation($"*** \n \n *** Our Three File: ");
             var ohd = fileContent[0]; 
             var oli = fileContent[1]; 
-            var pi = fileContent[2]; 
+            var pi = fileContent[2];
+            var prefix = fileContent[3];  
 
-            log.LogInformation("Ohd : " + ohd);  
+            log.LogInformation("ohd : " + ohd);  
             log.LogInformation("oli : " + oli);  
             log.LogInformation("pi : " + pi);  
+            log.LogInformation("bundle prefix: " + prefix); 
 
             // Get all the files with this prefix: 
             // {prefix}-OrderHeaderDetails.csv
@@ -62,7 +59,7 @@ namespace Company.Function
 
             var storageCredentials = new StorageCredentials("myAccountName", "myAccountKey");
             
-            return $"Hello {prefix}!";
+            return $"Hello {ohd}!";
         }
 
         [FunctionName("DurableFunctionsOrchestrationCSharp_HttpStart")]
@@ -75,7 +72,7 @@ namespace Company.Function
 
             var queryValues = req.RequestUri.ParseQueryString();
             var prefix = queryValues["prefix"];
-            log.LogInformation(prefix);
+            // log.LogInformation(prefix);
 
 
             var orchestratorProcessStatus = await starter.GetStatusAsync(prefix);
@@ -121,13 +118,13 @@ namespace Company.Function
             }
 
 
-            if(orchestrationFunctionProcessStatus.InstanceId == prefixAndInstanceId) {
-                log.LogInformation("knocked out of the park \n prefixAndInstanceId: " + prefixAndInstanceId + " orchestrationFunctionProcessStatus.InstanceId: " + orchestrationFunctionProcessStatus.InstanceId);
-            }
+            // if(orchestrationFunctionProcessStatus.InstanceId == prefixAndInstanceId) {
+            //     log.LogInformation("knocked out of the park \n prefixAndInstanceId: " + prefixAndInstanceId + " orchestrationFunctionProcessStatus.InstanceId: " + orchestrationFunctionProcessStatus.InstanceId);
+            // }
 
 
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob} Bytes");
-            log.LogInformation("INSTANCE ID...: " + prefixAndInstanceId);
+            // log.LogInformation("INSTANCE ID...: " + prefixAndInstanceId);
 
             var stuffInBlob = myBlob; 
 
